7b39b7bc6065ad11f3dffb63b1e97c30068b95fa
FlorianChavernac <florian.chavernac@etud.univ-jfc.fr>
2023-02-08 15:26:02 +0100

    Update resolvers.js

--- backend/resolvers.js
+++ backend/resolvers.js
@@ -18,12 +18,36 @@ module.exports = {
         }
     },
     Mutation: {
-         acheterQtProduit(args, context) {
-            const product = context.world.products.find(
-                p => p.id == args.id)
+        acheterQtProduit(args, context) {
+            if (context.world.products.find(p => p.id == args.id) == null) {
+                throw new Error(
+                    `Le produit avec l'id ${args.id} n'existe pas`)
+            }
+            else {
+                context.world.products.find(p => p.id == args.id).quantite += args.quantite
+                //le prix n'est pas le même si pour chaque quantité
+                context.world.money -= args.quantite * context.world.products.find(p => p.id == args.id).cout
+            }
+            saveWorld(context)
+            return context.world
+        },
+
+        lancerProductionProduit(args, context) {
+            if (context.world.products.find(p => p.id == args.id) == null) {
+                throw new Error(
+                    `Le produit avec l'id ${args.id} n'existe pas`)
+            }
+            else {
+                context.world.products.find(p => p.id == args.id).timeleft = context.world.products.find(p => p.id == args.id).vitesse
+            }
+            saveWorld(context)
+            return context.world
+        },
+        engagerManager(args, context) {
+            context.world.managers.find(m => m.name == args.name).unlocked = true
+            context.world.products.find(p => p.id == context.world.managers.find(m => m.name == args.name).idcible).managerUnlocked = true
             saveWorld(context)
             return context.world
         }
-
     }
-};
+}
