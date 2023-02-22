7745097bf85cd52f336e89df1a5d996d983f3960
Manonmouric <manon.mouric@gmail.com>
2023-02-22 10:17:00 +0100

    22/02

--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { World } from './world'
+import { Product, World } from './world'
 import ProductComponent from './Product'
 import {transform} from "./utils";
 
@@ -12,7 +12,15 @@ function Main({ loadworld, username }: MainProps) {
     const [world, setWorld] = useState(JSON.parse(JSON.stringify(loadworld)) as
         World)
 
-
+        function addToScore(gain: number): void {
+            world.money += gain
+        }
+        function onProductionDone(p: Product, nb: number): void {
+            // calcul de la somme obtenue par la production du produit
+            let gain =p.revenu * p.quantite * (1 + world.activeangels * world.angelbonus / 100)*nb
+            // ajout de la somme à l’argent possédé
+            addToScore(gain)
+        }
     useEffect(() => {
         setWorld(JSON.parse(JSON.stringify(loadworld)) as World)
     }, [loadworld])
@@ -21,12 +29,13 @@ function Main({ loadworld, username }: MainProps) {
     return (<div><img src={"http://localhost:4000/" + world.logo} />
         <span> {world.name} </span>
         <span dangerouslySetInnerHTML={{ __html: transform(world.money) }} />
-        <ProductComponent product={world.products[0]} />
-        <ProductComponent product={world.products[1]} />
-        <ProductComponent product={world.products[2]} />
-        <ProductComponent product={world.products[3]} />
-        <ProductComponent product={world.products[4]} />
-        <ProductComponent product={world.products[5]} /></div>
+        <ProductComponent product={world.products[0]} onProductionDone={onProductionDone}/>
+        <ProductComponent product={world.products[1]} onProductionDone={onProductionDone}/>
+        <ProductComponent product={world.products[2]} onProductionDone={onProductionDone}/>
+        <ProductComponent product={world.products[3]} onProductionDone={onProductionDone}/>
+        <ProductComponent product={world.products[4]} onProductionDone={onProductionDone}/>
+        <ProductComponent product={world.products[5]} onProductionDone={onProductionDone}/></div>
     )
+
 }
 export default Main;
--- frontend/src/Product.tsx
+++ frontend/src/Product.tsx
@@ -1,18 +1,66 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import MyProgressBar from './MyProgressBar';
+import  { useInterval } from './MyInterval';
+import MyProgressbar, { Orientation} from './MyProgressBar';
 import "./product.css"
+import "./MyProgressBar.css"
 import {transform} from "./utils";
 import { Product } from './world'
 type ProductProps = {
     product: Product
+    onProductionDone: (product: Product, nb: number) => void
     }
 
-function ProductComponent({ product} : ProductProps) {
+//hook de type ref
+function ProductComponent(this: any, { product, onProductionDone} : ProductProps) {
+    useInterval(() => calcScore(), 100)
+    const lastupdate=useRef(Date.now())
+    const [timeleft, setTimeleft] = useState(product.timeleft);
+     
         return (
-        <div>  
-            <img  src={"http://localhost:4000/" + product.logo} className="round"/>
+        <div> 
+            <img onClick={startFabrication} src={"http://localhost:4000/" + product.logo} className="round"/>
             <div className="lesdeux">
 <div className="lepremier"> <span> {product.quantite} </span></div>
 <div className="lesecond"> <span> {product.name} </span></div>
-</div> </div>
-)}
+<div className="ligne">
+<MyProgressbar vitesse={product.vitesse} initialvalue={product.vitesse - timeleft} className="barstyle"  run={run()} frontcolor="#6e7a70" backcolor="#ffffff"
+                    auto={product.managerUnlocked}
+                    orientation={Orientation.horizontal} />
+<div className="temps">hello</div></div></div></div>
+)
+function startFabrication(){
+    console.log("startFabrication")
+}
+function run(){
+    if (timeleft > 0||product.managerUnlocked) {
+    return true}
+    else return false
+}
+function calcScore(){   
+//calcule le temps écoulé depuis la dernière sauvegarde
+let now = Date.now()
+let tempsecoule = now - lastupdate.current;
+if (!product.managerUnlocked) {
+        //Le temps écoulé est assez grand donc la production est fini
+        if (timeleft > 0 && timeleft < tempsecoule) {
+            console.log("Le produit ", product.name, " a réussi à créer un exemplaire")
+            setTimeleft(0);
+          onProductionDone(product,1);
+        } else {
+            //le temps écoulé n'est pas assez grand pour finir la production, on met à jour le temps restant
+            if (product.timeleft > 0) {
+                setTimeleft(timeleft - tempsecoule);
+            }
+        }
+    }//Ceux qui ont manager
+    else {
+        //combien de fois vitesse a pu se produire
+        let nb = Math.floor(tempsecoule / product.vitesse)
+        console.log("Le produit ", product.name, " a produit ", nb, " exemplaires")
+        setTimeleft(product.vitesse - (tempsecoule % product.vitesse))
+        onProductionDone(product,nb);
+    }
+    lastupdate.current = now}
+}
 export default ProductComponent;
